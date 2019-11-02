c509d695134c2a76b206a64ae9df960ad35ee949
Ji-Hyeon <46284684+Ji-Hyeon@users.noreply.github.com>
2019-11-02 22:05:05 +0900

    Initial commit

---
+++ Main/CS/Main.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using OpenQA.Selenium.IE;
+using System.Windows.Forms;
+
+namespace Main
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+    }
+}
