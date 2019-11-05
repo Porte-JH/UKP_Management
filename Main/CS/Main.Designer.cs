7526f68f2b52ef3ce5e6ef09916bdf43a7aa9a72
Ji-Hyeon <46284684+Ji-Hyeon@users.noreply.github.com>
2019-11-05 11:23:35 +0900

    DB processing

--- Main/CS/Main.Designer.cs
+++ Main/CS/Main.Designer.cs
@@ -28,20 +28,206 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(123, 33);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(150, 54);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(123, 108);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(150, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(123, 147);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '*';
+            this.textBox2.Size = new System.Drawing.Size(150, 21);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(68, 117);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(61, 156);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "PW";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(173, 223);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 21);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "로그인";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(286, 250);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(88, 16);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "로그인 저장";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "종합정보시스템",
+            "출석체크",
+            "NCS"});
+            this.comboBox1.Location = new System.Drawing.Point(123, 184);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(150, 20);
+            this.comboBox1.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(48, 192);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 12);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Service";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(279, 223);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 21);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "불러오기";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(21, 227);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 12);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Check_Num";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(123, 223);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(35, 21);
+            this.textBox3.TabIndex = 11;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 328);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 21);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "로그인 기록";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(173, 250);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 21);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "강의실 조회";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(258, 337);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(114, 12);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Porte,JH / 21814012";
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(384, 361);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
             this.Name = "Main";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Main_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- Main/CS/Main.cs
+++ Main/CS/Main.cs
@@ -10,14 +10,185 @@ using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium;
 using OpenQA.Selenium.IE;
 using System.Windows.Forms;
-
+using System.Threading;
+using System.Data.SQLite;
 namespace Main
 {
+
     public partial class Main : Form
     {
+        private SQLiteConnection conn = null;
         public Main()
         {
+
             InitializeComponent();
+
         }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+
+            string DbFile = "MyDatabase.dat";
+            string ConnectionString = string.Format("Data Source={0};Version=3;", DbFile);
+
+            try
+            {
+                if (!System.IO.File.Exists(DbFile))
+                {
+                    SQLiteConnection.CreateFile(DbFile);  // SQLite DB 생성
+                }
+                else  // 기능 동작 여부 확인을 위해서 추가했지만 불필요
+                {
+                    MessageBox.Show("DB 생성되어 있습니다");
+                    return;
+                }
+
+                // 테이블 생성 코드
+                SQLiteConnection sqliteConn = new SQLiteConnection(ConnectionString);
+                sqliteConn.Open();
+
+                string strsql = "CREATE TABLE IF NOT EXISTS DATA (Id varchar(20), pw varchar(20))";
+
+                SQLiteCommand cmd = new SQLiteCommand(strsql, sqliteConn);
+                cmd.ExecuteNonQuery();
+                sqliteConn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return;
+            }
+            
+        }
+
+
+        public void Login_Sys(string input_id, string input_pw, IWebDriver sys_driver)
+        {
+
+            sys_driver.Url = "https://auth.kimpo.ac.kr/html/login.php";
+            IWebElement sys_id = sys_driver.FindElement(By.Name("id"));
+            sys_id.SendKeys(input_id);
+            sys_driver.FindElement(By.Name("pw")).SendKeys(input_pw);
+            sys_driver.FindElement(By.XPath("//*[@id='wrapper_login']/form/div/table[3]/tbody/tr[1]/td[3]/img")).Click();
+            sys_driver.Url = "http://kpis4.kimpo.ac.kr:8000/hosting/group/kpis/html/kpis_main.php";
+
+            sys_driver.FindElement(By.LinkText("정보광장")).Click();
+        }
+
+        public void Login_Check(string input_id, string input_pw, IWebDriver check_driver)
+        {
+            check_driver.Url = "https://check.kimpo.ac.kr";
+            IWebElement check_id = check_driver.FindElement(By.XPath("//*[@id='username']"));
+            check_id.SendKeys(input_id);
+            check_driver.FindElement(By.XPath("//*[@id='passwd']")).SendKeys(input_pw);
+            check_driver.FindElement(By.XPath("//*[@id='loginform']/button")).Click();
+        }
+
+        public void Login_Ncs(string input_id, string input_pw, IWebDriver ncs_driver)
+        {
+
+            ncs_driver.Url = "https://ncs.kimpo.ac.kr/login";
+            IWebElement ncs_id = ncs_driver.FindElement(By.XPath("//*[@id='loginId']"));
+            ncs_id.SendKeys(input_id);
+            ncs_driver.FindElement(By.XPath("//*[@id='password']")).SendKeys(input_pw);
+
+            ncs_driver.FindElement(By.XPath("/html/body/div[1]/div/div/div/div/div[2]/form/div/label[2]")).Click();
+            ncs_driver.FindElement(By.XPath("//*[@id='loginBtn']")).Click();
+        }
+
+        public void Check(IWebDriver check_driver, string link_value)
+        {
+            var check_num = textBox3.Text;
+            check_driver.Url = "https://check.kimpo.ac.kr/index.php/student/AttendCheck/" + link_value;
+            //MessageBox.Show(check_driver.Url);
+            Thread.Sleep(1000);
+            check_driver.FindElement(By.XPath("/html/body/div[1]/div[2]/div[1]/ul[1]/li[2]/span[3]/div/div[1]/div/input")).SendKeys(check_num);
+            check_driver.FindElement(By.XPath("/html/body/div[1]/div[2]/div[1]/ul[1]/li[2]/span[3]/div/button")).Click();
+        }
+
+        public void Save_Data(string id, string pw, SQLiteConnection sqliteConn, SQLiteCommand cmd)
+        {
+            if(checkBox1.Checked == true)
+            {
+                sqliteConn.Open();
+                string sql = "insert into members (id, pw) values ('" + id + "', '" + pw + "')";
+                cmd = new SQLiteCommand(sql, sqliteConn);
+                cmd.ExecuteNonQuery();
+                sqliteConn.Close();
+            }
+        }
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            
+            var input_id = textBox1.Text;
+            var input_pw = textBox2.Text;
+            int Now_Hour = int.Parse(DateTime.Now.ToString("hh"));
+
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                    IWebDriver sys_driver = new InternetExplorerDriver();
+                    Login_Sys(input_id, input_pw, sys_driver);
+                    //Save_Data(input_id, input_pw, sqliteConn, cmd);
+                    break;
+                case 1:
+                    IWebDriver check_driver = new ChromeDriver();
+                    Login_Check(input_id, input_pw, check_driver);
+
+                    if (textBox3.Text != "")
+                    {
+                        string link_value;
+                        check_driver.Url = "https://check.kimpo.ac.kr/index.php/student/main";
+                        if (Now_Hour == 9)
+                        {
+                            IWebElement First_Class = check_driver.FindElement(By.XPath("//*[@id='schedule']/ul/li[1]"));
+                            link_value = First_Class.GetAttribute("lid");
+                            First_Class.Click();
+                            Check(check_driver, link_value);
+
+                        }
+                        else if (Now_Hour == 1)
+                        {
+                            IWebElement Second_Class = check_driver.FindElement(By.XPath("//*[@id='schedule']/ul/li[4]"));
+                            link_value = Second_Class.GetAttribute("lid");
+                            Second_Class.Click();
+                            Check(check_driver, link_value);
+
+                        }
+                        else if (Now_Hour == 4)
+                        {
+                            IWebElement Third_Class = check_driver.FindElement(By.XPath("//*[@id='schedule']/ul/li[7]"));
+                            link_value = Third_Class.GetAttribute("lid");
+                            Third_Class.Click();
+                            Check(check_driver, link_value);
+
+                        }
+                        else
+                        {
+                            MessageBox.Show("출석체크가 가능한 시간이 아닙니다.");
+                        }
+
+                    }
+
+                    break;
+                case 2:
+                    IWebDriver ncs_driver = new ChromeDriver();
+                    Login_Ncs(input_id, input_pw, ncs_driver);
+                    break;
+                default:
+                    MessageBox.Show("항목을 선택해주세요...!");
+                    break;
+            }
+
+
+
+        }
+
+        
     }
 }
+
